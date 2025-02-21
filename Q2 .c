b84d03cdb6de0d88b58fad898e95dbbf12f504da
Tasmiah Ahammed Novely <113685249+tasmiahnovely@users.noreply.github.com>
2025-02-22 00:08:45 +0600

    Update and rename Main (1).c to Q2 .c

---
+++ Q2 .c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    char c = 'a';
+
+
+        for (c = 'A'; c <= 'z'; ++c) {
+
+                        printf("%c  and ASCII code %d\n", c, c );
+
+
+        };
+
+
+
+
+
+
+
+    return 0;
+}
+
+
